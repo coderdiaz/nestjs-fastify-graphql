2b6904089ca203629763383a515fc43ec0f0ca0f
Javier Diaz <coderdiaz@gmail.com>
2022-05-21 00:35:53 -0500

    feat: loaded controller in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 import config from './config';
 
 @Module({
+  controllers: [AppController],
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
   ],
